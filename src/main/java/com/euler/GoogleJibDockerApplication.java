3142ecee42f254ba0bbdb275fc6f172f3e01aac6
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2024-02-24 19:39:24 -0500

    Google job docker

---
+++ src/main/java/com/euler/GoogleJibDockerApplication.java
@@ -0,0 +1,21 @@
+package com.euler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GoogleJibDockerApplication {
+
+
+    @GetMapping("/message")
+    public String message(){
+        return "Hello Google Jib for Docker.";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleJibDockerApplication.class, args);
+    }
+
+}
